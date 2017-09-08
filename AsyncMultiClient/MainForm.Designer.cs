276f33e01aa95ed32916cb245b25cced33121729
Lei Tang <tang.lei@ensco.com>
2017-09-08 10:16:40 -0400

    added new project

--- AsyncMultiClient/MainForm.Designer.cs
+++ AsyncMultiClient/MainForm.Designer.cs
@@ -31,15 +31,15 @@
             this.buttonConnect = new System.Windows.Forms.Button();
             this.buttonDisconnect = new System.Windows.Forms.Button();
             this.buttonSend = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
             this.radioButtonMale = new System.Windows.Forms.RadioButton();
             this.radioButtonFemale = new System.Windows.Forms.RadioButton();
             this.textBoxIP = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonConnect
@@ -61,6 +61,7 @@
             this.buttonDisconnect.TabIndex = 1;
             this.buttonDisconnect.Text = "Disconnect";
             this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
             // 
             // buttonSend
             // 
@@ -71,20 +72,21 @@
             this.buttonSend.TabIndex = 2;
             this.buttonSend.Text = "Send";
             this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
-            // textBox1
+            // textBoxName
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 108);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(155, 20);
-            this.textBox1.TabIndex = 3;
+            this.textBoxName.Location = new System.Drawing.Point(76, 108);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(155, 20);
+            this.textBoxName.TabIndex = 3;
             // 
-            // numericUpDown1
+            // numericUpDownAge
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(76, 184);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
-            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDownAge.Location = new System.Drawing.Point(76, 184);
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDownAge.TabIndex = 4;
             // 
             // radioButtonMale
             // 
@@ -155,14 +157,14 @@
             this.Controls.Add(this.textBoxIP);
             this.Controls.Add(this.radioButtonFemale);
             this.Controls.Add(this.radioButtonMale);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDownAge);
+            this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.buttonDisconnect);
             this.Controls.Add(this.buttonConnect);
             this.Name = "MainForm";
             this.Text = "ESI";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,8 +175,8 @@
         private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Button buttonDisconnect;
         private System.Windows.Forms.Button buttonSend;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
         private System.Windows.Forms.RadioButton radioButtonMale;
         private System.Windows.Forms.RadioButton radioButtonFemale;
         private System.Windows.Forms.TextBox textBoxIP;
--- AsyncMultiClient/MainForm.cs
+++ AsyncMultiClient/MainForm.cs
@@ -61,9 +61,33 @@ namespace AsyncMultiClient
                 if (received == 0) return;
                 clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
             }
-            catch
+            catch (SocketException ex)
             {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch ( Exception ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                clientSocket.EndSend(ar);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -84,6 +108,71 @@ namespace AsyncMultiClient
                     buttonSend.Enabled = false;
                 }
             });
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                PersonPackage person = new PersonPackage(radioButtonMale.Checked, (ushort)numericUpDownAge.Value, textBoxName.Text);
+                byte[] buffer = person.ToByteArray();
+                clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch ( ObjectDisposedException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Disconnect(false);
+               // clientSocket.Close();              
+                UpdateGui(false);
+            }
+            catch( SocketException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    class PersonPackage
+    {
+        public bool IsMale { get; set; }
+        public ushort Age { get; set; }
+        public string Name { get; set; }
+
+        public PersonPackage(bool male, ushort age, string name)
+        {
+            IsMale = male;
+            Age = age;
+            Name = name;
+        }
+
+        public byte[] ToByteArray()
+        {
+            List<byte> byteList = new List<byte>();
+            byteList.AddRange(BitConverter.GetBytes(IsMale));
+            byteList.AddRange(BitConverter.GetBytes(Age));
+            byteList.AddRange(BitConverter.GetBytes(Name.Length));
+            byteList.AddRange(Encoding.ASCII.GetBytes(Name));
+            return byteList.ToArray();
+        }
+
+        public PersonPackage(byte[] data)
+        {
+            IsMale = BitConverter.ToBoolean(data, 0);
+            Age = BitConverter.ToUInt16(data, 1);
+            int nameLength = BitConverter.ToInt32(data, 3);
+            Name = Encoding.ASCII.GetString(data, 7, nameLength);
         }
     }
 }
